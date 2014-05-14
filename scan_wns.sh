ff3d06466819a0b12c19b628f0d8af05ff9d40f1
Daniela Bauer <daniela.bauer.grid@googlemail.com>
2014-05-14 17:14:02 +0100

    write outout to file

--- scan_wns.sh
+++ scan_wns.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+echo "This will only work if you have root kerberos access to these nodes ..."
+sleep 2
+DATE=`date +%s`
+
+mv cmsblah.txt  cmsblah.txt.${DATE} > /dev/null 2>&1
+
 # wg
 
-for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wg0$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wg0$n ". /vols/grid/cms_glexec.sh"; done >>  cmsblah.txt 2>&1
 
-for n in `seq -w 10 69` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wg$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 10 69` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wg$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
 # we
 
-for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we00$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we00$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
-for n in `seq -w 10 99` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we0$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 10 99` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we0$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
 # we115 and 116 are not in the cluster
-for n in `seq -w 100 114` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 100 114` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
-for n in `seq -w 117 119` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 117 119` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@we$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
 # wf
 
-for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wf0$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 0 9` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wf0$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
 
-for n in `seq -w 10 35` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wf$n ". /vols/grid/cms_glexec.sh"; done
+for n in `seq -w 10 35` ; do ssh -o ConnectTimeout=30 -o StrictHostKeyChecking=no root@wf$n ". /vols/grid/cms_glexec.sh"; done  >>  cmsblah.txt 2>&1
+
