100c2bf70085201d363170bb356b32d45e22ba30
Daniela Bauer <daniela.bauer.grid@googlemail.com>
2014-05-14 16:01:21 +0100

    added help function and option to sort by crabid

--- cms_user.py
+++ cms_user.py
@@ -9,27 +9,38 @@ import sys
 def findOccurences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
 
+def print_help():
+    print "Usage:"
+    print "cms_user.py -h: print this help"
+    print "cms_user.py [no arguments]: prints overview"
+    print "cms_user.py -w [wnname]: prints all info for given WN"
+    print "cms_user -r: prints raw data (debugging option)"
+    print "cms_user -o [wn/dn/crabid]"
 
 def main(argv):
     
     
-    wnonly='' # testing on an empty string not ideal
-    print_this_overview='' # if in doubt print wn and dn
+    wnonly='' # if set, print information for this WN only
+    print_this_overview='' # if in doubt print all possible overviews (dn/wn/prj)
     print_raw_data=False
+   
+    # arguments are: -h (help), -r rawdata, -w (workernode, needs argument)
+    # -o type of overview to print (needs argument)
     try:
-        opts, args = getopt.getopt(argv,"hrw:o:") # arguments -h for 'help' , -w for 'worker node'
+        opts, args = getopt.getopt(argv,"hrw:o:") 
         
     except getopt.GetoptError:
-        print 'basic_text.py [-w name_of_workernode]'
+        print_help()
         sys.exit(2)
         
+      
       # if no arguments, this loop is never entered, hence no 'else' required    
     for opt, arg in opts:
         if opt == '-h':
-            print 'basic_text.py [-w name_of_workernode]'
+            print_help()
+            sys.exit(0)
         elif opt == '-w': 
             wnonly = arg
-            # print 'Looking at '+wnonly
         elif opt == '-r':
             print 'Printing raw data'
             print_raw_data=True
@@ -42,21 +53,19 @@ def main(argv):
     #  open file
     with open("cmsblah.txt") as rawfile:
         rawdata= rawfile.readlines()
-        # rawdata.read().splitlines()
+     # for debugging    
     if print_raw_data == True:     
         for line in rawdata:    
             print line
 
+    # read all jobs into a list
+    # jobs are stored as tuples (jobid, wn, userid, crabid, dn)
     cmsjobs = []   # list
     with open("cmsblah.txt") as rawfile:
         data = [line.split() for line in rawfile]
     for line in data:
-        # print line
         # print line[0],  line[1],  line[2],  line[3]
         # print len(line)
-     
-        # make a tuple
-        # this is jobid, wn, userid, crabid, dn
         # exclude debugging line ('Very old directory ...')
         # and jobs that failed the Sanity check
         if line[0] != 'Very' and line[0] != 'Sanity':
@@ -84,7 +93,7 @@ def main(argv):
             job=(line[1],  line[0],  line[2], crabidtmp, dn)
             cmsjobs.append(job)
 
-
+    # print jobs for selected WN only
     if wnonly != '':
         # print 'All jobs for '+wnonly
         jobsonthiswn=[]
@@ -99,10 +108,11 @@ def main(argv):
         for jobid, userid, crabid,dn in jobsonthiswn:
              print jobid+': '+userid+'\t'+crabid+'\t'+dn+'\n'         
 
-    # no arguments given, print overview
-    if wnonly == '':
+    # generate overview: number of jobs per WN/DN/CRABID
+    if wnonly == '':    
         jobs_per_wn = {} # ohh dictionary        
         jobs_per_dn = {}
+        jobs_per_crabid = {}
         for jobid, wn, userid, crabid,dn in cmsjobs:
             # print wn
             # count the number of jobs by WN
@@ -116,7 +126,11 @@ def main(argv):
                 jobs_per_dn[dn] = 1
             else:
                 jobs_per_dn[dn] = jobs_per_dn[dn]+1
-
+                
+            if crabid not in jobs_per_crabid:
+                jobs_per_crabid[crabid] = 1
+            else:
+                jobs_per_crabid[crabid] =  jobs_per_crabid[crabid]+1
 
         
         # now loop over the result
@@ -128,7 +142,9 @@ def main(argv):
             for dn,n_of_jobs in sorted(jobs_per_dn.iteritems()):
                 print dn, n_of_jobs
 
-
+        if  print_this_overview == '' or print_this_overview == 'crabid':   
+            for crabid,n_of_jobs in sorted(jobs_per_crabid.iteritems()):
+                print crabid, n_of_jobs
 
    
     
